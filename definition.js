6ea48e31d1a55f047b8ea8f64a9a82735caa23c3
simonerscott <rhoniscott@yahoo.com>
2016-11-10 16:18:28 +0000

    Exercise 2: Look up Any Word: Completed

--- definition.js
+++ definition.js
@@ -5,17 +5,30 @@ $(document).ready(function()
     var searchButton = $(".search");
     var url = "request.php?q=";
     
-    searchButton.click(function()
+    searchButton.on('click', aFunction);
+
+    function aFunction(event)
     {
-        //alert(defn.html());  
-         $.ajax({
-        url: url + "definition",
+        //The click event is passed to the function so that we can 
+        // prevent the default action of the button
+        event.preventDefault();
+      
+        // Will only get the value of termEntered if the searchButton is clicked, 
+        // since it's now within the function called on click.
+        var termEntered = $("#term").val();
+
+        $.ajax({
+          url: url + termEntered,
         })
         .done(function(data) 
         {
-            alert($(data).text()); 
+            //alert($(data).text()); 
+            //alert(data);
+            $( "#result" ).html(data);
+        }).fail(function()// In the event request is not able to find request page
+        {
+            alert("ERROR");
         });
-    });
-    
+    }
     
 });
